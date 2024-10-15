9fdd047b18fb8a4074d90a6c8f997f169bc8cb4c
Angel Vhugie <Angel Vhugie@DESKTOP-T6KBT5Q>
2024-10-15 23:20:24 +0200

    Add price and item array

---
+++ src/main/java/com/mycompany/priceitem/PriceItem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.priceitem;
+
+/**
+ *
+ * @author Angel Vhugie
+ */
+public class PriceItem {
+
+    public static void main(String[] args) {
+        // Arrays for price and items
+        int[] price = {200, 350, 700, 400};
+        String[] item = {"Shirt", "Pants", "Sneakers", "Jacket"};
+        
+    }
+}
